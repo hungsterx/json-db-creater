756a1e53364af48c8915e8f1e7321fdd14bb251d
hungsterx <hungsterx@gmail.com>
2017-01-11 17:50:31 +0100

    Added click listener
    created createDatabase and openDatabase

--- JsonDBCreator/app/src/main/java/com/hung/lexiconitkonsult/jsondbcreator/JsonDBCreatorMain.java
+++ JsonDBCreator/app/src/main/java/com/hung/lexiconitkonsult/jsondbcreator/JsonDBCreatorMain.java
@@ -1,12 +1,16 @@
 package com.hung.lexiconitkonsult.jsondbcreator;
 
-import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class JsonDBCreatorMain extends AppCompatActivity {
+
+    private final String TAG = "JsonDBCreatorMain";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +18,18 @@ public class JsonDBCreatorMain extends AppCompatActivity {
         setContentView(R.layout.activity_json_dbcreator_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.mainoptions, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void createDatabase(MenuItem item) {
+        Log.d("Create database pressed", TAG);
+    }
+
+    public void openDatabase(MenuItem item) {
+        Log.d("Open database pressed", TAG);
     }
 }
