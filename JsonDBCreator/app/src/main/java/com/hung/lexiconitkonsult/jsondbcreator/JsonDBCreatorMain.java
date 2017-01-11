d914b5373d4bbf95114e5f8c27644eb248c747bb
hungsterx <hungsterx@gmail.com>
2017-01-11 13:23:50 +0100

    The menu xml rmeoved
    menu must be created in res/menu folder

--- JsonDBCreator/app/src/main/java/com/hung/lexiconitkonsult/jsondbcreator/JsonDBCreatorMain.java
+++ JsonDBCreator/app/src/main/java/com/hung/lexiconitkonsult/jsondbcreator/JsonDBCreatorMain.java
@@ -1,7 +1,10 @@
 package com.hung.lexiconitkonsult.jsondbcreator;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class JsonDBCreatorMain extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class JsonDBCreatorMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json_dbcreator_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainoptions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
