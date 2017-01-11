fb572e29af52514d93570124aadbbee6f555f01d
hungsterx <hungsterx@gmail.com>
2017-01-11 11:32:15 +0100

    Initial project created
    Add a menuoption xml file to main

---
+++ JsonDBCreator/app/src/main/java/com/hung/lexiconitkonsult/jsondbcreator/JsonDBCreatorMain.java
@@ -0,0 +1,13 @@
+package com.hung.lexiconitkonsult.jsondbcreator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JsonDBCreatorMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_dbcreator_main);
+    }
+}
